68b2d48885211ce784edef5f761c6980bb66a249
gulkaiyr13 <toktomushewa@gmail.com>
2024-03-25 18:27:45 +0600

    feat: adding other aspects

--- src/main/java/com/example/springmid/controllers/ProductController.java
+++
@@ -1,4 +0,0 @@
-package com.example.springmid.controllers;
-
-public class ProductController {
-}
--- src/main/java/com/example/springmid/entities/Product.java
+++ src/main/java/com/example/springmid/entities/Product.java
@@ -1,18 +1,15 @@
 package com.example.springmid.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
+@Table(name = "products")
 @Data
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String name;
     private double price;
     private String description;
